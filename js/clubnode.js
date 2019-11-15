cf3c8fbb125a7ac9fbc643f2d7c4b39b297ffc52
Hans-Jurgen Greiner <hgrein@2019-iMac.lan>
2019-11-15 06:19:15 -0700

    Added a draw method to clubnode.js

--- js/clubnode.js
+++ js/clubnode.js
@@ -1,4 +1,62 @@
 
+  function draw() {
+
+    var now = Date.now();
+    var mod = 1;
+
+    ctx.globalCompositeOperation = "source-over";
+
+    if ( BLUR ) ctx.globalAlpha = 0.1; else if ( !BLUR && !BLINK ) ctx.globalAlpha = 1.0;
+
+    ctx.fillStyle = BACKGROUND;
+    ctx.fillRect( 0, 0, W, H );
+
+    if ( BLENDING ) ctx.globalCompositeOperation = "lighter";
+
+    var p = null;
+
+    for ( var i = 0; i < particles.length; i++ ) {
+      p = particles[i];
+
+      /* in lower qualities there is not enough full pixels for all of  them - dirty hack*/
+
+      if ( isNaN( p.x ) ) {
+        continue;
+      }
+
+      ctx.beginPath();
+      ctx.fillStyle = "rgb(" + p.r + ", " + p.g + ", " + p.b + ")";
+      ctx.fillStyle = "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.alpha + ")";
+
+
+      if ( BLINK ) ctx.globalAlpha = Math.sin( Math.PI * mod );
+
+      if ( PULSATION ) { /* this would be 0 -> 1 */
+        mod = ((GLOBAL_PULSATION ? 0 : p.deltaOffset) + now) % PULSATION_PERIOD / PULSATION_PERIOD;
+
+        /* lets make the value bouncing with sinus */
+        mod = Math.sin( mod * Math.PI );
+      } else {
+        mod = 1;
+      }
+
+      var offset = TREMBLING ? TREMBLING * (-1 + Math.random() * 2) : 0;
+
+      var radius = PARTICLE_RADIUS * p.radius;
+
+      if ( !ARC ) {
+        ctx.fillRect( offset + p.x - mod * radius / 2 | 0, offset + p.y - mod * radius / 2 | 0, radius * mod, radius * mod );
+      } else {
+        ctx.arc( offset + p.x | 0, offset + p.y | 0, radius * mod, 0, Math.PI * 2, false );
+        ctx.fill();
+      }
+
+
+      p.x += (p.dx - p.x) / 10;
+      p.y += (p.dy - p.y) / 10;
+    }
+  }
+
   function get_destinations() {
     var pa = null;
     var po = null;
