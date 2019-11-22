ca0b066b84016df431322c595740055d94213136
Hans-Jurgen Greiner <hgrein@2019-iMac.lan>
2019-11-21 20:54:48 -0700

    inital checkin of app.js node server

---
+++ app.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+
+app.get('/',function(req,res){
+
+//      return res.redirect('/public/index.html');
+      return res.sendFile('index.html', {root: __dirname });
+
+    });
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+//app.use('/public', express.static(__dirname + '/public'));
+
+/** Implementing Simple Music Server using Express JS **/
+app.get('/music', function(req,res){
+        // File to be served
+
+      var fileId = req.query.id;
+      var file = __dirname + '/music/' + fileId;
+      fs.exists(file,function(exists){
+            if(exists)
+            {
+              var rstream = fs.createReadStream(file);
+              rstream.pipe(res);
+            }
+            else
+            {
+              res.send("Its a 404");
+              res.end();
+            }
+
+          });
+
+    });
+app.get('/download', function(req,res){
+      var fileId = req.query.id;
+      var file = __dirname + '/music/' + fileId;
+      fs.exists(file,function(exists){
+            if(exists)
+            {
+              res.setHeader('Content-disposition', 'attachment; filename=' + fileId);
+              res.setHeader('Content-Type', 'application/audio/mpeg3')
+                  var rstream = fs.createReadStream(file);
+              rstream.pipe(res);
+            }
+            else
+            {
+              res.send("Its a 404");
+              res.end();
+            }
+          });
+
+
+    });
+
+app.get('/list', function(req,res){
+      console.log('List is called');
+
+
+      var fileId = req.query.id;
+      var path = __dirname + '/music';
+      var jsonRes = '';
+
+      fs.readdir(path, function(err,items){
+              /*
+                console.log(items);
+                for(var i = 0; i < items.length; i++ ) {
+                console.log(`item[${i}] = ${items[i]}`);
+                }
+
+              */
+            jsonRes += `{ "list" : "/music", "items" : [`;
+                                                          //jsonRes += "{ 'list' : '/music', 'items' : [";
+            for(var i = 0; i < items.length; i++ ) {
+              jsonRes += `"${items[i]}"`;
+              jsonRes += ( i < items.length - 1 ) ? "," : "";
+            }
+            
+//            jsonRes = "{ list : '/music', list: [" + items + "] }";
+            jsonRes += "] }";
+            console.log(jsonRes);
+            res.send(jsonRes);
+            res.end();
+
+          });
+
+    });
+
+
+app.listen(3003,function(){
+      console.log('App listening on port 3003!');
+    });
